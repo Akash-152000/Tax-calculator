21838631d42272e9977e7f2e0fce3cbba90f1133
Akash <57212963+Akash-152000@users.noreply.github.com>
2021-07-26 01:00:42 +0530

    Added some font color

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Welcome</h1>
-      <h1>Here you can calcualte your Tax rate and net income</h1>
+      <h1>Here you can calcualte your <span className="green">Tax rate</span> and <span className="green">Gross income</span></h1>
       <div>
           <CentralCard/>
       </div>
